50d87aa0af1710250484ade049eafd91d1b9ff1b
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-14 01:25:48 -0400

    indicate that 'amount' shouldn't be considered part of the public API of Dollar

--- money/dollar.py
+++ money/dollar.py
@@ -1,10 +1,10 @@
 class Dollar():
 
     def __init__(self, amount):
-        self.amount = amount
+        self._amount = amount
 
     def __eq__(self, other):
-        return self.amount == other.amount
+        return self._amount == other._amount
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self._amount * multiplier)
