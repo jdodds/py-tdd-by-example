91fae464d98cce17b9b17bf83fa1ca759f81ba67
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-14 05:33:49 -0400

    Use our Money.dollar factory method everywhere in our tests.

--- tests/test_money.py
+++ tests/test_money.py
@@ -7,8 +7,8 @@ from money.currencies import Money, Dollar, Franc
 class DollarsTest(TestCase):
     def testMultiplication(self):
         five = Money.dollar(5)
-        self.assertEqual(Dollar(10), five.times(2))
-        self.assertEqual(Dollar(15), five.times(3))
+        self.assertEqual(Money.dollar(10), five.times(2))
+        self.assertEqual(Money.dollar(15), five.times(3))
 
     def testFrancMultiplication(self):
         five = Franc(5)
@@ -16,11 +16,11 @@ class DollarsTest(TestCase):
         self.assertEqual(Franc(15), five.times(3))
 
     def testEquality(self):
-        self.assertEqual(Dollar(5), Dollar(5))
-        self.assertNotEqual(Dollar(5), Dollar(6))
+        self.assertEqual(Money.dollar(5), Money.dollar(5))
+        self.assertNotEqual(Money.dollar(5), Money.dollar(6))
         self.assertEqual(Franc(5), Franc(5))
         self.assertNotEqual(Franc(5), Franc(6))
-        self.assertNotEqual(Franc(5), Dollar(5))
+        self.assertNotEqual(Franc(5), Money.dollar(5))
 
 
 if __name__ == '__main__':
