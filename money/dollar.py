3b36c3c0389a07997d85aa7c6219bf68d4470c7d
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-14 04:57:15 -0400

    remove Franc's constructor and _amount variable

--- money/dollar.py
+++ money/dollar.py
@@ -10,10 +10,6 @@ class Dollar(Money):
         return Dollar(self._amount * multiplier)
 
 class Franc(Money):
-
-    def __init__(self, amount):
-        self._amount = amount
-
     def __eq__(self, other):
         return self._amount == other._amount
 
