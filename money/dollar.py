a1ff6af2d5d8a5f27a7a3110d72cfb6cef9ed070
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-13 21:51:29 -0400

    initialize amount to the value given in the constructor, and utilize the multiplier given in the times method like a sane person

--- money/dollar.py
+++ money/dollar.py
@@ -1,7 +1,7 @@
 class Dollar():
 
     def __init__(self, amount):
-        self.amount = None
+        self.amount = amount
 
     def times(self, multiplier):
-        self.amount = 10
+        self.amount *= multiplier
