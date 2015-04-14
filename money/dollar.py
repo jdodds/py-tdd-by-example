f48b2e485cd4f41db0f258d5e4ce570c15b6aae7
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-13 23:01:44 -0400

    implement equality as naively as possible

--- money/dollar.py
+++ money/dollar.py
@@ -3,5 +3,8 @@ class Dollar():
     def __init__(self, amount):
         self.amount = amount
 
+    def __eq__(self, other):
+        return True
+
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
