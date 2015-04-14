24f11a79767fdd2b14ca45a0b8ef6a7e0297d171
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-14 04:35:54 -0400

    Add a test for Francs, a currency we want to be able to handle

--- tests/test_dollars.py
+++ tests/test_dollars.py
@@ -10,6 +10,11 @@ class DollarsTest(TestCase):
         self.assertEqual(Dollar(10), five.times(2))
         self.assertEqual(Dollar(15), five.times(3))
 
+    def testFrancMultiplication(self):
+        five = Franc(5)
+        self.assertEqual(Franc(10), five.times(2))
+        self.assertEqual(Franc(15), five.times(3))
+
     def testEquality(self):
         self.assertEqual(Dollar(5), Dollar(5))
         self.assertNotEqual(Dollar(5), Dollar(6))
