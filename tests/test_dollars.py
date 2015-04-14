4234602ae4ad4b1e9d49a72af7da2b91f4756534
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-13 22:05:19 -0400

    we want a Dollar to represent a *dollar*, not a changeable number of dollars, so we write a test that will fail if the 'amount' of a particular dollar changes over time

--- tests/test_dollars.py
+++ tests/test_dollars.py
@@ -7,8 +7,10 @@ from money.dollar import Dollar
 class DollarsTest(TestCase):
     def testMultiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
 
 if __name__ == '__main__':
     unittest.main()
