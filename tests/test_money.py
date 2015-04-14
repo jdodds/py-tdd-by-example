fac23bd7688a26c3ae6e8224d6b0ffffe48607e4
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-14 05:36:43 -0400

    give Franc the same treatment we just gave Dollar in our tests

--- tests/test_money.py
+++ tests/test_money.py
@@ -11,16 +11,16 @@ class DollarsTest(TestCase):
         self.assertEqual(Money.dollar(15), five.times(3))
 
     def testFrancMultiplication(self):
-        five = Franc(5)
-        self.assertEqual(Franc(10), five.times(2))
-        self.assertEqual(Franc(15), five.times(3))
+        five = Money.franc(5)
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
 
     def testEquality(self):
         self.assertEqual(Money.dollar(5), Money.dollar(5))
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))
-        self.assertEqual(Franc(5), Franc(5))
-        self.assertNotEqual(Franc(5), Franc(6))
-        self.assertNotEqual(Franc(5), Money.dollar(5))
+        self.assertEqual(Money.franc(5), Money.franc(5))
+        self.assertNotEqual(Money.franc(5), Money.franc(6))
+        self.assertNotEqual(Money.franc(5), Money.dollar(5))
 
 
 if __name__ == '__main__':
