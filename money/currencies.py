d6170c883cfcabebc1450bba48c82e77d35c0197
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-14 05:13:50 -0400

    solve our Dollar <-> Franc comparison problem by checking types in __eq__, introducing some smell to our code

--- money/currencies.py
+++ money/currencies.py
@@ -3,7 +3,10 @@ class Money():
         self._amount = amount
 
     def __eq__(self, other):
-        return self._amount == other._amount
+        return (
+            type(self) == type(other) and
+            self._amount == other._amount
+        )
 
 class Dollar(Money):
     def times(self, multiplier):
