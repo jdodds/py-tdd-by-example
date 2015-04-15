8d9873c80f135ca8eb4025460966115cc314af60
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-15 19:18:45 -0400

    we can remove testFrancMultiplication without losing any confidence that our system runs correctly, so let's go ahead and do that

--- tests/test_currencies.py
+++ tests/test_currencies.py
@@ -10,11 +10,6 @@ class CurrencyTest(TestCase):
         self.assertEqual(Money.dollar(10), five.times(2))
         self.assertEqual(Money.dollar(15), five.times(3))
 
-    def testFrancMultiplication(self):
-        five = Money.franc(5)
-        self.assertEqual(Money.franc(10), five.times(2))
-        self.assertEqual(Money.franc(15), five.times(3))
-
     def testEquality(self):
         self.assertEqual(Money.dollar(5), Money.dollar(5))
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))
