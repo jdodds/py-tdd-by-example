e2c02aae26bc4ec09c2465a385d15123c15921da
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-16 10:26:18 -0400

    mark money.amount as being intended for 'public' use

--- finance/money.py
+++ finance/money.py
@@ -2,17 +2,17 @@ from .total import Total
 
 class Money():
     def __init__(self, amount, currency):
-        self._amount = amount
+        self.amount = amount
         self._currency = currency
 
     def __eq__(self, other):
         return (
             self.currency() == other.currency() and
-            self._amount == other._amount
+            self.amount == other.amount
         )
 
     def __repr__(self):
-        return "Money(%s, %s)" % (self._amount, self._currency)
+        return "Money(%s, %s)" % (self.amount, self._currency)
 
     @staticmethod
     def dollar(amount):
@@ -23,7 +23,7 @@ class Money():
         return Money(amount, 'CHF')
 
     def times(self, multiplier):
-        return Money(self._amount * multiplier, self._currency)
+        return Money(self.amount * multiplier, self._currency)
 
     def currency(self):
         return self._currency
