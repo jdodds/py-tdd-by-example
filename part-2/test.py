5d7f46c6f2c57d9c0b4af17db09e9a65b1fd024e
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 04:39:23 -0400

    add a setUp method to TestCase and call setUp in TestCase.run

--- part-2/test.py
+++ part-2/test.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
