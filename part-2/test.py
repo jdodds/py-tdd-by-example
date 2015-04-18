e0a70ef040d29d2660db942d361eb9e7b7e92b19
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 01:56:13 -0400

    add a stub WasRun.testMethod

--- part-2/test.py
+++ part-2/test.py
@@ -2,6 +2,9 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def testMethod(self):
+        pass
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
