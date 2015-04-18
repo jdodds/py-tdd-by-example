f11905bd7f0748428d6c10cd64bfb2ca66168e8b
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 04:50:09 -0400

    delete testRunning and rename testSetUp

--- part-2/test.py
+++ part-2/test.py
@@ -27,11 +27,7 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert("setUp testMethod " == self.test.log)
 
