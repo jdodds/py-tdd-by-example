fd6d3cb28b2d4b6c41051a7873cc859e804ef800
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 04:52:29 -0400

    test that we call a tearDown method

--- part-2/test.py
+++ part-2/test.py
@@ -27,6 +27,6 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
