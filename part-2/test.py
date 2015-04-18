caf70e46a9533c9046dabe5d1b41019d59938040
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 05:01:11 -0400

    add a failing test for a test summary

--- part-2/test.py
+++ part-2/test.py
@@ -36,4 +36,10 @@ class TestCaseTest(TestCase):
         test.run()
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
