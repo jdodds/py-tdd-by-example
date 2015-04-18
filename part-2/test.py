9e76e347a3d12c5a340ca02d029aa5415e949d08
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 04:46:36 -0400

    test the log in testSetUp

--- part-2/test.py
+++ part-2/test.py
@@ -33,7 +33,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp" == self.test.log)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
