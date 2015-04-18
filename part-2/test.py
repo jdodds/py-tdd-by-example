5b83b99d6e90d63c86314e5c0229900ce32139cc
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 05:13:49 -0400

    add a more finely grained test for failed result formatting

--- part-2/test.py
+++ part-2/test.py
@@ -62,6 +62,13 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
