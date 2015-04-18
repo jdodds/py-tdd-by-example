1a5fcce87238f5eb5faa0478cb5c89b5ad30a2b7
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 02:02:57 -0400

    move name up into TestCase

--- part-2/test.py
+++ part-2/test.py
@@ -1,10 +1,11 @@
 class TestCase:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
