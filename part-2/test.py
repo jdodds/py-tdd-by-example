ee220cdc1c706dd2d965e3f962ff7c8112da8106
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 02:01:00 -0400

    dynamically invoke the method passed to WasRun's constructor

--- part-2/test.py
+++ part-2/test.py
@@ -1,12 +1,14 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def testMethod(self):
         self.wasRun = 1
 
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
 
 test = WasRun("testMethod")
 print(test.wasRun)
