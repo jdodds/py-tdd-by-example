58ead1117dd97568215be3eaff0b3b467d5a2019
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 05:23:11 -0400

    add a testSuite method

--- part-2/test.py
+++ part-2/test.py
@@ -75,6 +75,13 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert("1 run, 1 failed" == result.summary())
 
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = suite.run()
+        assert("2 run, 1 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 TestCaseTest("testFailedResult").run()
