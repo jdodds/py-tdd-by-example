c47c07ebe1cbb8252b79771ad25c64fbaf4e3922
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 05:15:24 -0400

    record errors in TestResult

--- part-2/test.py
+++ part-2/test.py
@@ -20,9 +20,13 @@ class TestCase:
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
     def summary(self):
         return "%d run, 0 failed" % self.runCount
