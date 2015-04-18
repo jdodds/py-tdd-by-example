8fceafb711d277a30f343cc6dbef7e234174aad0
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 04:41:44 -0400

    remove the now unneccessary check from testRunning

--- part-2/test.py
+++ part-2/test.py
@@ -25,7 +25,6 @@ class WasRun(TestCase):
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
 
