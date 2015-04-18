c0a3675a225f75bd2639dff827c1acc34776babc
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-04-18 05:04:38 -0400

    give TestResult a way to increment it's runCount variable

--- part-2/test.py
+++ part-2/test.py
@@ -17,7 +17,10 @@ class TestCase:
 
 class TestResult:
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
 
     def summary(self):
         return "%d run, 0 failed" % self.runCount
